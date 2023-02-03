a3db7d22aaf57cd00485db577132944585482782
manikanta700 <81902024+manikanta700@users.noreply.github.com>
2023-02-03 16:39:03 +0530

    Create Binary Tree Preorder Traversal.java

---
+++ 24-01-2023/Binary Tree Preorder Traversal.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+        preOrder(root,ans);
+        return ans;
+
+
+        
+    }
+    public void preOrder(TreeNode node,List ans){
+        if(node==null)return ;
+        ans.add(node.val);
+        preOrder(node.left,ans);
+        preOrder(node.right,ans);
+
+    }
+}
