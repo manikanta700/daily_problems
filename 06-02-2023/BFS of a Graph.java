6df3cad72a6032e7d11f3c5061c87cd7c39de641
manikanta700 <81902024+manikanta700@users.noreply.github.com>
2023-02-06 16:08:37 +0530

    Create BFS of a Graph.java

---
+++ 06-02-2023/BFS of a Graph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> res  = new ArrayList<>();
+        Queue<Integer> Q = new LinkedList<>();
+        boolean[] visted = new boolean[V];
+        Q.offer(0);
+        res.add(0);
+        visted[0]=true;
+        
+        
+        while(!Q.isEmpty()){
+            int i = Q.poll();
+            for(int j : adj.get(i)){
+                if(!visted[j]){
+                    
+                Q.offer(j);
+                visted[j]=true;
+                res.add(j);
+                }
+            }
+        }
+        return res;
+    }
+}
